53a8ef670b83622ae82c71b372b43e6951a5b0e4
ismetalp98 <alp.eren@ug.bilkent.edu.tr>
2021-12-26 18:25:29 +0300

    çalışmayabilir

--- SurePa_Frontend/src/components/addMedicinePage/AddMedicinePage.js
+++ SurePa_Frontend/src/components/addMedicinePage/AddMedicinePage.js
@@ -1,68 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import SearchableDropdown from 'react-native-searchable-dropdown';
 import { db } from '../../services/DbCon';
+import DropDown from '../dropDown/DropDown';
 
 const AddMedicinePage = ({ navigation }) => {
     const [serverData, setServerData] = useState([]);
+    const [selectedItem, setSelectedItem] = useState(undefined);
 
     useEffect(() => {
         const arr = [];
         let id = 0;
         db.collection("medicines").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                arr.push({id: id++, name: doc.data().Medicine_name});
+                arr.push({ id: `${doc.id}/${doc.data().Doze_count}`, name: doc.data().Medicine_name });
             });
             setServerData(arr);
         });
     }, []);
+
+    useEffect(() => {
+        if (selectedItem) {
+            console.log(selectedItem);
+            const arr = [];
+            // db.collection("medicines").doc(selectedItem.id).get().then((querySnapshot) => {
+            //     console.log(querySnapshot);
+            //     querySnapshot.forEach((doc) => {
+            //         // arr.push({ id: id++, name: doc.data().Medicine_name });
+            //         console.log(doc);
+            //     });
+            //     setServerData(arr);
+            // });
+        }
+    }, [selectedItem]);
 
 
     return (
         <View>
             <Text> AddMedicinePag </Text>
             <View>
-                <SearchableDropdown
-                    onTextChange={(text) => console.log(text)}
-                    //On text change listner on the searchable input
-                    onItemSelect={(item) => alert(JSON.stringify(item))}
-                    //onItemSelect called after the selection from the dropdown
-                    containerStyle={{ padding: 5 }}
-                    //suggestion container style
-                    textInputStyle={{
-                        //inserted text style
-                        padding: 12,
-                        borderWidth: 1,
-                        borderColor: '#ccc',
-                        backgroundColor: '#FAF7F6',
-                    }}
-                    itemStyle={{
-                        //single dropdown item style
-                        padding: 10,
-                        marginTop: 2,
-                        backgroundColor: '#FAF9F8',
-                        borderColor: '#bbb',
-                        borderWidth: 1,
-                    }}
-                    itemTextStyle={{
-                        //text style of a single dropdown item
-                        color: '#222',
-                    }}
-                    itemsContainerStyle={{
-                        //items container style you can pass maxHeight
-                        //to restrict the items dropdown hieght
-                        maxHeight: '50%',
-                    }}
-                    items={serverData}
-                    //mapping of item array
-                    defaultIndex={2}
-                    //default selected item index
-                    placeholder="placeholder"
-                    //place holder for the search input
-                    resetValue={false}
-                    //reset textInput Value with true and false state
-                    underlineColorAndroid="transparent"
-                //To remove the underline from the android input
+                <DropDown
+                    serverData={serverData}
+                    setSelectedItem={setSelectedItem}
+                    selectedItem={selectedItem}
                 />
             </View>
 
---
+++ SurePa_Frontend/src/components/dropDown/DropDown.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import SearchableDropdown from 'react-native-searchable-dropdown';
+
+const DropDown = ({
+    setSelectedItem,
+    serverData,
+    selectedItem
+}) => {
+    return (
+        <SearchableDropdown
+            onTextChange={(item) => null}
+            //On text change listner on the searchable input
+            onItemSelect={(item) => setSelectedItem(item)}
+            //onItemSelect called after the selection from the dropdown
+            containerStyle={{ padding: 5 }}
+            //suggestion container style
+            textInputStyle={{
+                //inserted text style
+                padding: 12,
+                borderWidth: 1,
+                borderColor: '#ccc',
+                backgroundColor: '#FAF7F6',
+            }}
+            itemStyle={{
+                //single dropdown item style
+                padding: 10,
+                marginTop: 2,
+                backgroundColor: '#FAF9F8',
+                borderColor: '#bbb',
+                borderWidth: 1,
+            }}
+            itemTextStyle={{
+                //text style of a single dropdown item
+                color: '#222',
+            }}
+            itemsContainerStyle={{
+                //items container style you can pass maxHeight
+                //to restrict the items dropdown hieght
+                maxHeight: '80%',
+            }}
+            items={serverData}
+            //mapping of item array
+            defaultIndex={1}
+            //default selected item index
+            placeholder="placeholder"
+            //place holder for the search input
+            resetValue={true}
+            //reset textInput Value with true and false state
+            underlineColorAndroid="transparent"
+        //To remove the underline from the android input
+        />
+    )
+};
+
+export default DropDown;
